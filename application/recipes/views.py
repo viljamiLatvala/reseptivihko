f3ce89c12d147524816948bef490df4bf6e30185
Latvala Viljami <latvavil@lx7-fuxi057.ad.helsinki.fi>
2018-12-27 16:19:56 +0200

    views for individual recipes and edition functionality added

--- application/recipes/views.py
+++ application/recipes/views.py
@@ -10,11 +10,28 @@ def recipes_index():
 def recipes_form():
     return render_template("recipes/new.html")
 
+@app.route("/recipes/<recipe_id>/", methods=["GET"])
+def recipe_info(recipe_id):
+    return render_template("recipes/recipe.html", recipe = Recipe.query.get(recipe_id))
+
+@app.route("/recipes/<recipe_id>/edit/", methods=["GET"])
+def recipe_editform(recipe_id):
+    return render_template("recipes/edit.html", recipe = Recipe.query.get(recipe_id))
+
+@app.route("/recipes/<recipe_id>/", methods=["POST"])
+def recipe_edit(recipe_id):
+    r = Recipe.query.get(recipe_id)
+    r.name = request.form.get("name")
+    r.instruction = request.form.get("instruction")
+
+    db.session().commit()
+
+    return redirect(url_for("recipes_index"))
+
 @app.route("/recipes/", methods=["POST"])
 def recipes_create():
-    print(request.form.get("instruction"))
-    r = Recipe(request.form.get("name", "instruction"))
-    print(r)
+    r = Recipe(request.form.get("name"))
+    r.instruction = request.form.get("instruction")
     db.session().add(r)
     db.session().commit()
 
