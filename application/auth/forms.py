d829a4c64533e6df8dad719ba196a0f8a31dc365
Latvala Viljami <latvavil@lx7-fuxi057.ad.helsinki.fi>
2019-01-02 17:22:48 +0200

    sign up form validation, checking for duplicate users

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
   
+    class Meta:
+        csrf = False
+
+class SignupForm(FlaskForm):
+    name = StringField("Name",[validators.Length(min=3)])
+    username = StringField("Username",[validators.Length(min=3)])
+    password = PasswordField("Password",[validators.Length(min=8)])
+
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, SignupForm
+
+from sqlalchemy import func
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -22,4 +24,27 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
-    return redirect(url_for("index")) 
+    return redirect(url_for("index"))
+
+@app.route("/auth/signup", methods = ["GET", "POST"])
+def auth_signup():
+    if request.method == "GET":
+        return render_template("auth/signupform.html", form = SignupForm())
+
+    form = SignupForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/signupform.html", form = form)
+    
+    nameTaken = User.query.filter(func.lower(User.username) == func.lower(form.username.data)).first()
+    if nameTaken:
+        form.username.errors.append('usename taken!')
+        return render_template("auth/signupform.html", form = form)
+
+    newUser = User(form.name.data,form.username.data,form.password.data)
+    db.session().add(newUser)
+    db.session().commit()
+
+    createdUser = User.query.filter(username=form.username.data, password=form.password.data).first()
+    login_user(createdUser)
+    return redirect(url_for("index"))
