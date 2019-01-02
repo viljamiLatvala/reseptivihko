ac1559c5eb991b6fe0ec7543113c34816607eb8f
Latvala Viljami <latvavil@lx7-fuxi057.ad.helsinki.fi>
2019-01-02 15:27:36 +0200

    increased character count for recipe instructions

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -7,7 +7,7 @@ class Recipe(db.Model):
     onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
-    instruction = db.Column(db.String(144), nullable=False)
+    instruction = db.Column(db.String(6000), nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
