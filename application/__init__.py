a6a0566a1512171028f7f0fd8f240a71ea0e44a2
Latvala Viljami <latvavil@lx7-fuxi057.ad.helsinki.fi>
2019-01-02 15:12:12 +0200

    more heroku configs

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,8 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 else:
