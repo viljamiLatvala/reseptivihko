37b75bc798cb2b0f92c3f94a6cef8f9b74014a08
Latvala Viljami <latvavil@lx7-fuxi057.ad.helsinki.fi>
2019-01-04 17:31:24 +0200

    Form styling

--- application/recipes/forms.py
+++ application/recipes/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, IntegerField, validators
 
 class RecipeForm(FlaskForm):
     name = StringField("Name", [validators.Length(min=3)])
-    ingredients = TextAreaField("ingredients", [validators.Length(min=10)])
+    ingredients = TextAreaField("Ingredients", [validators.Length(min=10)])
     preptime = IntegerField("Preparation time (minutes)", [validators.required()])
     instruction = TextAreaField("Instruction", [validators.Length(min=10)])
     tags = TextAreaField("Tags")
