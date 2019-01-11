dbc6fa471e83003313afdc5a81bca33eb7e8c12b
viljamiLatvala <32260281+viljamiLatvala@users.noreply.github.com>
2019-01-11 16:10:20 +0100

    Field validation updates
    Updated validators on forms' name and instructions fields, so that the variable lenghts set for the database models can not be exeeded. Validation for tags and ingredients fields remains to be done, as they are parsed into multiple database rows and thus validation must be done elsewhere.

--- application/recipes/forms.py
+++ application/recipes/forms.py
@@ -2,20 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, IntegerField, validators
 
 class RecipeForm(FlaskForm):
-    name = StringField("Name", [validators.Length(min=3)])
+    name = StringField("Name", [validators.Length(min=3, max=144)])
     ingredients = TextAreaField("Ingredients", [validators.Length(min=10)])
     preptime = IntegerField("Preparation time (minutes)", [validators.required()])
-    instruction = TextAreaField("Instruction", [validators.Length(min=10)])
+    instruction = TextAreaField("Instruction", [validators.Length(min=10, max=6000)])
     tags = TextAreaField("Tags")
 
     class Meta:
         csrf = False
 
 class RecipeEditForm(FlaskForm):
-    name = StringField("Name", [validators.Length(min=3)])
+    name = StringField("Name", [validators.Length(min=3, max=144)])
     ingredients = TextAreaField("ingredients", [validators.Length(min=10)])
     preptime = IntegerField("Preparation time (minutes)", [validators.required()])
-    instruction = TextAreaField("Instruction", [validators.Length(min=10)])
+    instruction = TextAreaField("Instruction", [validators.Length(min=10, max=144)])
     tags = TextAreaField("Tags")
  
     class Meta:
